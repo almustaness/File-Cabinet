88b10c89a5640a7312a89126834ba3d87a9c28ca
almustaness <e-comm@live.com>
2016-08-17 07:11:36 +0000

    User permissions to see only his docs and root is different for logged in users

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,7 +1,7 @@
 class DocsController < ApplicationController
   before_action :find_doc , only: [:edit, :update, :show, :destroy]
   def index
-    @docs = Doc.all.order("created_at DESC")
+    @docs = Doc.where(user_id: current_user)
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
+  
+  authenticated :user do
+    root "docs#index", as: "root_for_logged_user"
+  end
   resources :docs
+  root 'welcome#index'
+  
+
 end
